ad71dd2ad7cabd15fc7d57614d8982e1a5e1b05d
Anyhowclick <howeilun@hotmail.com>
2016-10-08 15:59:36 +0800

    Styled addpage

--- addpage.php
+++ addpage.php
@@ -9,7 +9,12 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrapValidator.css"/>
     <link href="assets/bootstrap/css/addpage.css" rel="stylesheet">
+
+    <script type="text/javascript" src="assets/jquery/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrapValidator.js"></script>
     <title>Add item-page</title> 
 </head>
 
@@ -38,53 +43,104 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres pas
           </div>
       </nav>
 
+  <div class="addItem">
+    <div class="row">
+        <!-- form: -->
+      <section>
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="page-header">
+            <h2><strong>Add new item</strong></h2>
+          </div>
 
-<table>
-<tr> <td colspan="2" style="background-color:#008B8B;">
-<h1> <font color=#FFFAF0>Add new item</font></h1>
-</td> </tr>
+      <form id="addItem" method="post" class="form-horizontal"
+        data-bv-feedbackicons-valid="glyphicon glyphicon-ok"
+        data-bv-feedbackicons-invalid="glyphicon glyphicon-remove"
+        data-bv-feedbackicons-validating="glyphicon glyphicon-refresh">
 
-<tr>
-<td style="background-color:#FFFAF0;">
-<form>
-        Item:     <input type="text" name="Itemname" id="Itemname" /></p>
-        Location: <input type="text" name="Location" id="Location" /></p>
-        Short description: <textarea name="Description" rows="3" cols="40"><?php echo $Description;?></textarea /></p>
-        Condition: <input type="radio" name="Condition" id="condition1" value="Excellent">Excellent
-        <input type="radio" name="Condition" id="condition2" value="Good">Good
-        <input type="radio" name="Condition" id="condition3" value="Poor">Poor
+        <div class="form-group">
+          <label class="col-lg-3 control-label">Item</label>
+          <div class="col-lg-6">
+            <input type="text" class="form-control" name="Itemname" placeholder="Item Name" required data-bv-notempty-message="Item name required"/>
+          </div>
+        </div>
 
-        </select/></p>
+        <div class="form-group">
+          <label class="col-lg-3 control-label">Location</label>
+          <div class="col-lg-6">
+            <input type="text" class="form-control" name="Location" placeholder="Location" required data-bv-notempty-message="Location required"/>
+          </div>
+        </div>
 
-        <input type="submit" name="formSubmit" value="Submit" >
-        <input type="button" name="cancel" value="Back to homepage" onClick="window.location.href='/homepage.php';" />
+        <div class="form-group">
+          <label class="col-lg-3 control-label">Description</label>
+          <div class="col-lg-6">
+            <textarea class="form-control" name="Description" rows="10" placeholder="Give a short description of your item (Recommended, max 500 chars)"
+              data-bv-stringlength data-bv-stringlength-max="500" data-bv-stringlength-message="Description is too long"></textarea>
+          </div>
+        </div>
 
-</form>
+        <div class="form-group">
+          <label class="col-lg-3 control-label">Condition</label>
+            <div class="col-lg-6">
+              
+              <div class="radio">
+                <label>
+                  <input type="radio" name="Condition" value="Excellent" /> Excellent 
+                </label>
+              </div>
+
+              <div class="radio">
+                <label>
+                  <input type="radio" name="Condition" value="Good" /> Good 
+                </label>
+              </div>
+
+
+              <div class="radio">
+                <label>
+                  <input type="radio" name="Condition" value="Poor" /> Poor 
+                </label>
+              </div>
+
+              <div class="form-group">
+                <div class="col-lg-12 col-lg-offset-5">
+                    <button type="submit" class="btn btn-success" name="formSubmit">Submit!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </form>
+
+
+
+
+
+
+
 <?php
-
-/*symlink ( string 'http://localhost:8080/book-demo-1.php' , string 'Homepage' );*/
-
-if(isset($_GET['formSubmit'])) //Always use POST instead of GET!
+if(isset($_POST['formSubmit'])) //Always use POST instead of GET!
 {
     $usermail = $_SESSION['emailaddress'];
     $query = "INSERT INTO Item(name, location, condition, owner)
-    VALUES('".$_GET['Itemname']."' , '".$_GET['Location']."' , '".$_GET['Condition']."' , '$usermail' )";
+    VALUES('".$_POST['Itemname']."' , '".$_POST['Location']."' , '".$_POST['Condition']."' , '$usermail' )";
     $result = pg_query($query) or die('You did not fill in the form properly, try again please!');
-    echo "<b></b>".'Your item has been added, thank you!'."<br><br>";
+    echo '<div class="alert alert-success">
+            Bid successfully placed!
+            </div>';
     /*pg_free_result($result);*/
 }
 ?>
 
-
-
-</td> </tr>
 <?php
 pg_close($dbconn);
 ?>
-<tr>
-<!--<td colspan="2" style="background-color:#008B8B; text-align:center;"> Lendstuff.com-->
-</td> </tr>
-</table>
 
 </body>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#addItem').bootstrapValidator();
+    });
+</script>
+
 </html>
